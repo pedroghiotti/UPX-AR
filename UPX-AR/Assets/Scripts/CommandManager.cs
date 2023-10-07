5dfbbb0b64e36424e8489b57b78acfea3cc52a18
Pedro Ghiotti Martins <pedro.ghiotti.m@gmail.com>
2023-10-07 11:06:30 -0300

    Image Targets substituídos por Vumarks
    Para permitir a detecção de várias imagens muito similares, implementei o uso de Vumarks substituíndo Image Targets no projeto.

---
+++ UPX-AR/Assets/Scripts/CommandManager.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+/*
+    Classe responsável por manter uma lista atualizada dos objetos trackeados pelo vuforia
+    e executar os comandos correspondentes à eles.
+*/
+public class CommandManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> tracked = new List<GameObject>();
+
+    public void AddTracked(GameObject obj) { tracked.Add(obj); }
+    public void RemoveTracked(GameObject obj) { tracked.Remove(obj); }
+
+    /*
+        A função Execute irá organizar os comandos da esquerda para a direita,
+        como estiverem dispostas fisicamente as imagens detectadas, e executá-los sequencialmente.
+
+        A implementação de condicionais aqui é bem protótipo do protótipo então vou ter que reescrever
+        a funcionalidade quando formos adicionar loops ou condicionais maiores. No momento, apenas pula ou executa
+        o comando diretamente após um condicional com base no seu retorno (false ou true). A condição deve ser
+        verificada diretamente no código do comando que deverá retornar o resultado
+        (ver Cmd_Conditional.cs como exemplo).
+
+        Esse trecho entre colchetes antes do método permite que seja executado do editor,
+        através de uma opção no menu contextual nos três pontinhos na extremidade direita do componente.
+    */
+    [ContextMenu("Execute")]
+    public async void Execute()
+    {
+        /*
+            Organizo em ordem da esquerda para a direita e busco, 
+            nos gameobjects, os componentes dos comandos.
+        */
+        var commandsQueue = tracked.OrderBy(obj => obj.transform.position.x).Select(obj => obj.GetComponent<Command>()).ToList();
+
+        for(int i = 0; i < commandsQueue.Count(); i++)
+        {
+            bool response = await commandsQueue[i].Execute();
+            if(!response) i++;
+        }
+    }
+}
---
+++ UPX-AR/Assets/Scripts/Commands/Cmd_Conditional.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+
+/*
+    Herda de Command.
+    Sobrescreve o método Execute, impementando uma função de Raycast;
+    Faz uma verificação, à partir da posição do jogador e em direção frontal dele,
+    procurando por obstruções. Retornando true caso encontre e false caso não encontre.
+    As obstruções são identificadas por seu layer. Para que um objeto seja detectado como obstrução,
+    deve ter um colisor e estar em um dos layers definidos na layermask.
+
+    A distância de detecção e os layers detectados são configuráveis através das variáves
+    raycastLength e layerMask.
+*/
+public class Cmd_Conditional : Command
+{
+    [Header("Settings")]
+    [SerializeField] private float raycastLength = 5;
+    [SerializeField] private LayerMask layerMask;
+
+    public override async Task<bool> Execute()
+    {
+        return Physics.Raycast(playerTransform.position, playerTransform.forward, raycastLength, layerMask);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(!Application.isPlaying) return;
+        
+        Gizmos.DrawRay(playerTransform.position, playerTransform.forward * raycastLength);
+    }
+}
